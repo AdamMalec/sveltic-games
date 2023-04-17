ec252923040084378003148588ff9100acd2db3c
adam malec <add.malec@gmail.com>
2023-04-17 18:04:08 +0300

    feat: add main menu (nav)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,3 +2,12 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Sveltic Games</title>
+</svelte:head>
+
+<main>
+	<div class="container">
+		<slot />
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="menu">
+	<ul class="menu__list">
+		<li class="menu__item"><a href="typing-race">Typing race</a></li>
+		<li class="menu__item"><a href="blood-money">Blood money (in process)</a></li>
+		<li class="menu__item"><a href="hop-tower">Hop tower (in process)</a></li>
+	</ul>
+</nav>
+
+<style>
+	.menu__list {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	.menu__item a {
+		display: inline-block;
+		padding: 0.8rem;
+		font-size: 2rem;
+		text-transform: uppercase;
+	}
+</style>
