ac2ccd5d26b7817096acf46a6ce6533c9cf71a9c
adam malec <add.malec@gmail.com>
2023-05-01 15:31:11 +0300

    init: add game entities

---
+++ src/routes/typing-race/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	type Game = 'Waiting for input' | 'In progress' | 'Game Over';
+	type Word = string;
+
+	let game: Game = 'Waiting for input';
+	let typedLetter = '';
+
+	let words: Word[] = 'A bird in the hand is worth two in the bush'.split(' ');
+	let wordIndex = 0;
+	let letterIndex = 0;
+	let correctLetters = 0;
+
+	let wordsEl: HTMLDivElement;
+	let letterEl: HTMLSpanElement;
+	let inputEl: HTMLInputElement;
+
+	function updateGameState() {}
+
+	function handleKeydown(event: KeyboardEvent) {}
+</script>
+
+<div class="game" data-game={game}></div>
