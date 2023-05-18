47b6d8b37cd1f10947aaff2aaad8d59204c444a7
adam malec <add.malec@gmail.com>
2023-05-18 11:41:01 +0300

    feat: add layout file for Typing Race

---
+++ src/routes/typing-race/+layout.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Sveltic Gam8s: Typing Race</title>
+</svelte:head>
+
+<section class="layout">
+	<h1>Typing Race</h1>
+	<slot />
+</section>
+
+<style>
+	.layout {
+		display: grid;
+		grid-template-rows: auto 1fr;
+		align-items: center;
+		padding: 2rem;
+	}
+</style>
